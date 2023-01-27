d0b26a50fe6bf5465d63169a08e779c975c9a3a0
micchambers <micolc76@gmail.com>
2023-01-27 00:41:35 -0500

    Game loop written. Once the game is started, attacks will repeat until one fighter loses all health and is defeated

--- battlefield.py
+++ battlefield.py
@@ -6,20 +6,31 @@ class Battleground:
 
     def __init__(self, name):
         self.name = name
-        self.dinosaur = Dinosaur("Hydrasaurus", 20)
-        self.robot = Robot("Poebot")
+        self.dinosaur = Dinosaur("Dino", 20)
+        self.robot = Robot("Robo")
 
     def run_game(self):
         self.display_welcome()
-        self.robot.attack(self.dinosaur)
-        self.dinosaur.attack(self.robot)
-
+        self.battle_phase()
+ 
     def display_welcome(self):
         print("Welcome to the " + self.name)
 
     def battle_phase(self):
-        pass
-
+        self.winner = "Undetermined"
+        while self.dinosaur.health > 0 and self.robot.health > 0:
+            self.robot.attack(self.dinosaur)
+            if self.dinosaur.health <= 0:
+                self.winner = self.robot.name
+                self.display_winner()
+                self.two_living_fighters = False
+            if self.dinosaur.health > 0:
+                self.dinosaur.attack(self.robot)
+                if self.robot.health <= 0:
+                    self.winner = self.dinosaur.name
+                    self.display_winner()
+                    self.two_living_fighters = False
+            
     def display_winner(self):
-        pass
+        print(f'The winner is {self.winner}!')
  
--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from dinosaur import Dinosaur
 
 from weapon import Weapon
 
-battleground_one = Battleground("RumbleDome")
+battleground_one = Battleground("Robot vs Dinosaur Battle!")
 
 battleground_one.run_game()
